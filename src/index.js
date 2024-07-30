9885cca480768d014d998ce95a50ac129aef17a8
Narvana <adhikarijagdish3@gmail.com>
2024-07-30 12:39:24 +0530

    Second Commit -: Database Integration With Mongodb

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 const express=require('express');
 const app=express();
 const bodyparser=require('body-parser');
-const cookieParser=require('cookie-parser');
+// const cookieParser=require('cookie-parser');
 
 // security
 const cors= require('cors');
 const helmet=require('helmet');
 
+// port
 const port=1010;
+
+
+// database
+require('./database/Warehouse.db');
+app.get('/test/database',(req,res)=>{
+ const isConnected= mongoose.connection.readyState===1;
+ if(isConnected){
+     res.status(201).json({message :'MongoDB connection is active'})
+ }
+ else{
+     res.status(500).json({message :'MongoDB connection is not active'})
+ }
+})
+
 
 app.get('/',(req,res)=>{
     res.send('Welcome to Warehouseing')
 })
+app.get('/test/port',(req,res,next)=>{
+    res.status(201).send(`Secure Connection with port ${port}`)
+})
 
-app.listen(post,()=>{
+app.listen(port,()=>{
     console.log(`Connection with port ${port}`);
 })
