fa5cea23b230e224cdc2ac24a5fe29dcc4a9cf8b
Narvana <adhikarijagdish3@gmail.com>
2024-08-10 15:34:39 +0530

    21th commit -:Verify Token Code updated

--- src/middleware/token/verifyToken.js
+++ src/middleware/token/verifyToken.js
@@ -1,6 +1,9 @@
 // require('dotenv').config();
 const jwt=require('jsonwebtoken')
+
 // const secretKey=process.env.SECRET
+const ApiErrors=require('../../utils/ApiResponse/ApiErrors');
+const ApiResponse=require('../../utils/ApiResponse/ApiResponse');
 
 // const cookieParser=require('cookie-parser')
 // app.use(cookieParser());
@@ -16,19 +19,19 @@ const verify=(req,res,next)=>{
     
         if(!token)
         {
-            return res.status(400).send('Unauthorized User, No Token Found')
+            return next(ApiErrors(401,'Unauthorized User, No Token Found'));
         }
         jwt.verify(token, 'xQJslU3ieVjhYt0xCUu8hhUGayx265KgfP4W0abHhvfJJA8xFO8cYVChPGhjz0JT4w1GP3vURXdXBk8jC2Hu4W49jz', (err,user)=>{
             if(err){
-                return res.status(400).send(`Unauthorized User, Token Don't Matches`,)
+                return next(ApiErrors(401,"Unauthorized User, Incorrect Token"));
             }
             req.user=user;
             next();
         })
     
     } catch (error) {
-        res.status(500).json({error})   
-    }
+        return next(ApiErrors(500,`Internal Server Error  -: ${error}`));
+    } 
 }
 
 module.exports={
