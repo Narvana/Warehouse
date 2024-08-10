130bc676b3ecef60c6346102243b22710924f3a9
Narvana <adhikarijagdish3@gmail.com>
2024-08-10 16:08:03 +0530

    27th commit -:Warehouse database Code updated

--- src/database/Warehouse.db.js
+++ src/database/Warehouse.db.js
@@ -2,15 +2,15 @@ const path = require('path');
 
 // require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-require('dotenv').config({ path: '../.env' });
+// require('dotenv').config({ path: '../.env' });
 
 const mongoose = require('mongoose');
-
-const uri = process.env.URI; 
+// 
+// const uri = process.env.URI;
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect('mongodb+srv://warehouse:1Ware2House3ing@cluster0.gkp8oym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('Successfully connected to MongoDB');
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
