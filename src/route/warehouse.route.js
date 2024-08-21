3bdf36573aabbb8f76919122ce5e37b846acc976
Narvana <adhikarijagdish3@gmail.com>
2024-08-21 23:07:33 +0530

    34th Warehouse.route.js updated

--- src/route/warehouse.route.js
+++ src/route/warehouse.route.js
@@ -10,12 +10,24 @@ const upload=require('../middleware/ImageUpload/imageUploadMiddleware')
 
 const warehouseController=require('../controller/warehouse.controller');
 
+// const threePLController=require('../controller/warehouse.controller')
+
 router.post('/add/warehouse',verifyToken.verify,verifyRole('WAREHOUSE'),upload.fields([{name:"wareHouseImage", maxCount:5}]),warehouseController.AddWareHouse);
-router.get('/get/WareHouse/all',warehouseController.allWareHouse);
-router.get('/get/WareHouse',verifyToken.verify,warehouseController.getWarehouse);
+router.get('/get/WareHouse/Lister/all',warehouseController.allWareHouse);
+router.get('/get/WareHouse',verifyToken.verify,warehouseController.getListerWarehouse);
 
 // single detail with query id
 router.get('/get/WareHouse/Detail',warehouseController.singleWareHouse);
+router.get('/get/WareHouse/All/Search',warehouseController.searchWareHouseAll);
+
+// 3PL
+// try {
+    // console.log('hello');
+    // router.post('/add/ThreePL',verifyToken.verify,verifyRole('WAREHOUSE'),upload.fields([{ name: 'warehouse_details.WarehouseImage', maxCount: 5 }]),warehouseController.Add3PL);
+
+// } catch (error) {
+//     console.log(error);
+// }
 
 
 module.exports=router
