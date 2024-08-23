27f495472ca3228d12e7f02050e280dc60bb8929
Narvana <adhikarijagdish3@gmail.com>
2024-08-23 16:29:23 +0530

    51th commit -:Warehouse route updated and 3PL/Warehouse and 3PL/Coldstorage added

--- src/route/warehouse.route.js
+++ src/route/warehouse.route.js
@@ -14,14 +14,15 @@ const warehouseController=require('../controller/warehouse.controller');
 
 router.post('/add/warehouse',verifyToken.verify,verifyRole('WAREHOUSE'),upload.fields([{name:"wareHouseImage", maxCount:5}]),warehouseController.AddWareHouse);
 
-router.get('/get/WareHouse/Lister/all',warehouseController.allWareHouse);
-router.get('/get/WareHouse',verifyToken.verify,warehouseController.getListerWarehouse);
+router.get('/get/WareHouse/all',warehouseController.allWareHouse);
+router.get('/get/WareHouse/Lister',verifyToken.verify,warehouseController.getListerWarehouse);
 
 // single detail with query id
 router.get('/get/WareHouse/Detail',warehouseController.singleWareHouse);
 router.get('/get/WareHouse/All/Search',warehouseController.searchWareHouseAll);
 
 router.post('/add/ThreePL',verifyToken.verify,verifyRole('WAREHOUSE'),upload.fields([{ name: 'warehouse_details[WarehouseImage]', maxCount: 5 }]),warehouseController.Add3PL);
-
+router.post('/add/ThreePL/Warehouse',verifyToken.verify,verifyRole('WAREHOUSE'),upload.fields([{ name: 'warehouse_details[WarehouseImage]', maxCount: 5 }]),warehouseController.Add3PLWarehouse);
+router.post('/add/ThreePL/Coldstorage',verifyToken.verify,verifyRole('WAREHOUSE'),warehouseController.Add3PLColdStorage);
 
 module.exports=router
