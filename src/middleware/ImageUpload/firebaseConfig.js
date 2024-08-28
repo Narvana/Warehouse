7860c742d9068e90dc3d15c899ba48c2b2ed1ad9
Narvana <adhikarijagdish3@gmail.com>
2024-08-28 15:00:33 +0530

    59th commit -: UploadBase64ToFirebase add to firebaseConfig

--- src/middleware/ImageUpload/firebaseConfig.js
+++ src/middleware/ImageUpload/firebaseConfig.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-
+const { Buffer } = require('buffer');
+const path = require('path');
 var serviceAccount = require("../../WarehouseServiceAccountKey.json"); // Assuming correct path
 
 admin.initializeApp({
@@ -12,6 +13,7 @@ admin.initializeApp({
 const uploadToFirebase = async (file) => {
     try {
         // return res.json(file);
+        
       const bucket = admin.storage().bucket(); // Reference to the storage bucket
       const fileRef = bucket.file(file.originalname); // Create a reference to the uploaded file in storage
       const fileStream = fileRef.createWriteStream({
@@ -48,8 +50,62 @@ const uploadToFirebase = async (file) => {
       throw error;
     }
   };
+
+const uploadBase64ToFirebase = async (base64Data) => {
+  try {
+    const bucket = admin.storage().bucket(); // Reference to the storage bucket
+
+    // Extract the base64 data and MIME type
+    const matches = base64Data.match(/^data:(.+);base64,(.+)$/);
+    if (!matches) {
+      throw new Error('Invalid base64 data');
+    }
+    
+    const mimeType = matches[1]; // Extracted MIME type
+    const base64String = matches[2]; // Extracted base64 string
+    
+    // Convert the base64 string to a buffer
+    const buffer = Buffer.from(base64String, 'base64');
+    
+    // Create a reference to the uploaded file in storage
+    const fileRef = bucket.file(`image_${Date.now()}`); 
+    
+    const fileStream = fileRef.createWriteStream({
+      metadata: {
+        contentType: mimeType // Set the correct MIME type
+      }
+    });
+
+    return new Promise((resolve, reject) => {
+      fileStream.on('error', (error) => {
+        console.error('Error uploading file:', error);
+        reject(error);
+      });
+
+      fileStream.on('finish', async () => {
+        try {
+          const downloadURL = await fileRef.getSignedUrl({
+            action: 'read',
+            expires: '01-01-2500' // Set expiration date as desired
+          });
+          console.log('File uploaded successfully:', downloadURL);
+          resolve(downloadURL[0]); // Resolve with the first download URL
+        } catch (error) {
+          console.error('Error getting signed URL:', error);
+          reject(error);
+        }
+      });
+
+      fileStream.end(buffer); // End the stream, sending the buffer data
+    });
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    throw error;
+  }
+};
   
 
 module.exports = {
-  uploadToFirebase
+  uploadToFirebase,
+  uploadBase64ToFirebase
 };
