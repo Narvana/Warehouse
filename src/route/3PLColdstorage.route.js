3fa1edcd11090478e3dc01496427e23c8e42c11d
Narvana <adhikarijagdish3@gmail.com>
2024-08-28 16:13:47 +0530

    64th commit -: 3PL ColdStorage Router Added

---
+++ src/route/3PLColdstorage.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router=express.Router();
+
+const verifyToken=require('../middleware/token/verifyToken');
+
+const verifyRole=require('../middleware/role/verifyRole');
+
+const upload=require('../middleware/ImageUpload/imageUploadMiddleware');
+
+const PLColdStorageController=require('../controller/3PLColdStorage.controller');
+
+// 3PL ColdStorage
+router.post('/add/ThreePL/Coldstorage',verifyToken.verify,verifyRole('WAREHOUSE'),upload.fields([{name:'cold_storage_details[ColdStorageImage]'}]),PLColdStorageController.Add3PLColdStorage);
+router.get('/get/3PL/ColdStorage/Lister/All',verifyToken.verify,PLColdStorageController.AllPLColdStorage);
+router.get('/get/3PL/ColdStorage/Lister/Single',verifyToken.verify,PLColdStorageController.singlePLColdStorage);
+router.put('/update/3PL/ColdStorage',verifyToken.verify,PLColdStorageController.UpdatePLColdStorage);
+router.delete('/delete/3PL/ColdStorage',verifyToken.verify,PLColdStorageController.DeletePLColdStorage);
+
+module.exports=router
