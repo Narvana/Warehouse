69de36de049f659fdfd7031a097b35e5bcdc48d8
Narvana <adhikarijagdish3@gmail.com>
2024-08-28 16:36:21 +0530

    67th commit -: Image Upload MiddleWare MULTER Updated

--- src/middleware/ImageUpload/imageUploadMiddleware.js
+++ src/middleware/ImageUpload/imageUploadMiddleware.js
@@ -30,7 +30,7 @@ const fileFilter=(req,file,cb) =>{
 const upload=multer({
     storage,
     limits:{
-        fileSize : 1024 * 1024 * 1
+        fileSize : 1024 * 1024 * 5
     }, 
     fileFilter: fileFilter
 })
