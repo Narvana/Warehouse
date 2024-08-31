39b2c842f38237f235f8c3e67ef43823979c0a39
Narvana <adhikarijagdish3@gmail.com>
2024-08-31 15:02:50 +0530

    76th commit -: verifyRole is updated

--- src/middleware/role/verifyRole.js
+++ src/middleware/role/verifyRole.js
@@ -1,9 +1,27 @@
-function authorizeRoles(role) {
-    let checkRole=role
-
+function verifyRole(role) {
     return (req, res, next) => {  
+        let checkRole=role;
         req.role=checkRole;
-      next();
+        next()
     };
   }
-  module.exports = authorizeRoles;
+
+  function verifyRoles(roles) {
+    return (req, res, next) => {
+      const userRole = req.user.role; // Assuming req.user contains the authenticated user's details
+  
+      if (roles.includes(userRole)) {
+        // req.role = userRole;
+        next();
+      } else {
+        return res.status(403).json({ message: 'Access denied: You do not have the required role.' 
+          
+        });
+      }
+    };
+  }
+  
+  module.exports ={
+    verifyRole,
+    verifyRoles
+  };
