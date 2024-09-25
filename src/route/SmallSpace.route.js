18132ed186142cd05329e19ae6360c966c48a215
Narvana <adhikarijagdish3@gmail.com>
2024-09-25 18:28:35 +0530

    145th commit -: Small Space route Updated Role Added in Verify Token and VerifyToken Error Updated

--- src/route/SmallSpace.route.js
+++ src/route/SmallSpace.route.js
@@ -6,19 +6,19 @@ const SmallSpaceController = require('../controller/SmallSpace.contoller');
 
 // Small Space
 
-// Add 3PL Coldstorage
+// Add Small Space
 router.post('/add/listing', verify(['LISTER,ADMIN']), upload.fields([{ name: "SmallSpaceImage", maxCount: 5 }]), SmallSpaceController.AddSmallSpace);
 
-// GET LISTER ALL 3PL Coldstorage
+// GET LISTER ALL Small Space
 router.get('/get/All/Listing', verify(['LISTER','ADMIN']), SmallSpaceController.getListerAllSmallSpace);
 
-// GET Single 3PL ColdStorage With provided ID
+// GET Single Small Space With provided ID
 router.get('/get/Single/Listing', SmallSpaceController.singleSmallSpace);
 
-// UPDATE 3PL ColdStorage Provide ID
+// UPDATE Small Space Provide ID
 router.put('/update/Listing', verify(['LISTER','ADMIN']), upload.fields([{ name: "SmallSpaceImage", maxCount: 5 }]), SmallSpaceController.updateSmallSpace);
 
-// DELETE 3PL ColdStorage Provide ID
+// DELETE Small Space Provide ID
 router.delete('/delete/Listing', verify(['ADMIN','LISTER']), SmallSpaceController.DeleteSmallSpace);
 
 module.exports = router;
