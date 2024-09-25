4a4416c69467e5023154d73203bd1423dd422636
Narvana <adhikarijagdish3@gmail.com>
2024-09-25 18:06:59 +0530

    142th commit -: 3PL Warehouse route Updated Role Added in Verify Token and VerifyToken Error Updated

--- src/route/3PLWarehouse.route.js
+++ src/route/3PLWarehouse.route.js
@@ -2,19 +2,27 @@ const express = require('express');
 
 const router=express.Router();
 
-const verifyToken=require('../middleware/token/verifyToken');
-
-const {verifyRoles}=require('../middleware/role/verifyRole');
+const {verify}=require('../middleware/token/verifyToken');
 
 const upload=require('../middleware/ImageUpload/imageUploadMiddleware')
 
 const PLWarehouseController=require('../controller/3PLWarehouse.controller');
 
 // 3PL Warehouse
-router.post('/add/ThreePL/Warehouse',verifyToken.verify,verifyRoles(['LISTER', 'ADMIN']),upload.fields([{ name: 'warehouse_details[WarehouseImage]', maxCount: 5 }]),PLWarehouseController.Add3PLWarehouse);
-router.get('/get/3PL/WareHouse/Lister/All',verifyToken.verify,PLWarehouseController.AllPLWarehouse);
+
+// Add 3PL Warehouse
+router.post('/add/ThreePL/Warehouse',verify(['LISTER', 'ADMIN']),upload.fields([{ name: 'warehouse_details[WarehouseImage]', maxCount: 5 }]),PLWarehouseController.Add3PLWarehouse);
+
+// GET LISTER ALL 3PL Warehouse
+router.get('/get/3PL/WareHouse/Lister/All',verify(['LISTER', 'ADMIN']),PLWarehouseController.AllPLWarehouse);
+
+// GET Single 3PL Warehouse With provided ID
 router.get('/get/3PL/WareHouse/Lister/Single',PLWarehouseController.singlePLWarehouse);
-router.put('/update/3PL/Warehouse',verifyToken.verify,PLWarehouseController.UpdatePLWarehouse);
-router.delete('/delete/3PL/Warehouse',verifyToken.verify,PLWarehouseController.DeletePLWarehouse);
+
+// UPDATE 3PL Warehouse Provide ID
+router.put('/update/3PL/Warehouse',verify(['LISTER', 'ADMIN']),PLWarehouseController.UpdatePLWarehouse);
+
+// DELETE 3PL Warehouse Provide ID
+router.delete('/delete/3PL/Warehouse',verify(['LISTER', 'ADMIN']),PLWarehouseController.DeletePLWarehouse);
 
 module.exports=router
