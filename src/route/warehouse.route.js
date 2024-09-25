9d554482929be32fb3d37baede96d8293e64e391
Narvana <adhikarijagdish3@gmail.com>
2024-09-25 17:55:35 +0530

    137th commit -: Warehouse Controller Updated Role Added in VerifyToken and Token error updated

--- src/route/warehouse.route.js
+++ src/route/warehouse.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router=express.Router();
 
-const verifyToken=require('../middleware/token/verifyToken');
+const {verify}=require('../middleware/token/verifyToken');
 
-const {verifyRoles}=require('../middleware/role/verifyRole');
+// const {verifyRoles}=require('../middleware/role/verifyRole');
 
 const upload=require('../middleware/ImageUpload/imageUploadMiddleware')
 
@@ -12,17 +12,17 @@ const warehouseController=require('../controller/warehouse.controller');
 
 
 // Warehouse
-router.post('/add/warehouse',verifyToken.verify,verifyRoles(['LISTER', 'ADMIN']),upload.fields([{name:"wareHouseImage", maxCount:5}]),warehouseController.AddWareHouse);
+router.post('/add/warehouse',verify(['LISTER', 'ADMIN']),upload.fields([{name:"wareHouseImage",  maxCount:5}]),warehouseController.AddWareHouse);
 
-router.get('/get/WareHouse/Lister',verifyToken.verify,warehouseController.getListerAllWarehouse);
+router.get('/get/WareHouse/Lister',verify(['LISTER', 'ADMIN']),warehouseController.getListerAllWarehouse);
 
 router.get('/get/WareHouse/Detail',warehouseController.singleWareHouse);
 
-router.put('/update/WareHouse',verifyToken.verify,warehouseController.UpdateWarehouse);
+router.put('/update/WareHouse',verify(['LISTER', 'ADMIN']),warehouseController.UpdateWarehouse);
 
-router.delete('/delete/WareHouse',verifyToken.verify,warehouseController.DeleteWarehouse);
+router.delete('/delete/WareHouse',verify(['LISTER', 'ADMIN']),warehouseController.DeleteWarehouse);
 
 // Other
-router.post('/add/ThreePL',verifyToken.verify,verifyRoles(['WAREHOUSE', 'ADMIN']),upload.fields([{ name: 'warehouse_details[WarehouseImage]', maxCount: 5 }]),warehouseController.Add3PL);
+// router.post('/add/ThreePL',verifyToken.verify,verifyRoles(['WAREHOUSE', 'ADMIN']),upload.fields([{ name: 'warehouse_details[WarehouseImage]', maxCount: 5 }]),warehouseController.Add3PL);
 
 module.exports=router
