d7830557b0442d7485b9b931698b5f954b23d6ba
Narvana <adhikarijagdish3@gmail.com>
2024-09-25 18:09:33 +0530

    143th commit -: LAND route Updated Role Added in Verify Token and VerifyToken Error Updated

--- src/route/Land.route.js
+++ src/route/Land.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router=express.Router();
 
-const verifyToken=require('../middleware/token/verifyToken');
+const {verify}=require('../middleware/token/verifyToken');
 
 const {verifyRoles}=require('../middleware/role/verifyRole');
 
@@ -11,10 +11,20 @@ const upload=require('../middleware/ImageUpload/imageUploadMiddleware');
 const LandController=require('../controller/Land.controller');
 
 // 3PL ColdStorage
-router.post('/add',verifyToken.verify,verifyRoles(['LISTER', 'ADMIN']),upload.fields([{name:'LandImage'}]),LandController.AddLand);
-router.get('/get/Lister/All',verifyToken.verify,LandController.AllLandLister);
+
+// Add LAND
+router.post('/add',verify(['LISTER', 'ADMIN']),upload.fields([{name:'LandImage'}]),LandController.AddLand);
+
+// GET LISTER ALL LAND
+router.get('/get/Lister/All',verify(['LISTER', 'ADMIN']),LandController.AllLandLister);
+
+// GET Single LAND With provided ID
 router.get('/get/Lister/Single',LandController.ListerSingleLand);
-router.put('/Update',verifyToken.verify,LandController.UpdateListedLand);
-router.delete('/Delete',verifyToken.verify,LandController.RemoveLand);
+
+// UPDATE LAND Provide ID
+router.put('/Update',verify(['LISTER', 'ADMIN']),LandController.UpdateListedLand);
+
+// DELETE LAND Provide ID
+router.delete('/Delete',verify(['LISTER','ADMIN']),LandController.RemoveLand);
 
 module.exports=router
