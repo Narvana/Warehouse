d157d258636bdaf6f21f92f09fcf75a59f871a6b
Narvana <adhikarijagdish3@gmail.com>
2024-09-25 17:20:14 +0530

    134th commit -: Register Router Updated SignUP Login Profile Update PasswordUpdate Updated

--- src/route/register.route.js
+++ src/route/register.route.js
@@ -6,18 +6,22 @@ const registerController=require('../controller/register.controller')
 
 const definerole=require('../middleware/role/defineRole');
 const {verifyRole}=require('../middleware/role/verifyRole');
-const verify=require('../middleware/token/verifyToken')
 
-router.post('/register/Warehouse',definerole('LISTER'),registerController.SignUp);
-router.post('/login/Warehouse',verifyRole('LISTER'),registerController.login);
+const {verify}=require('../middleware/token/verifyToken')
+
+router.post('/register/Lister',definerole('LISTER'),registerController.SignUp);
+
+router.post('/login/Lister',verifyRole('LISTER'),registerController.login);
 
 router.post('/register/Admin',definerole('ADMIN'),registerController.SignUp);
+
 router.post('/login/Admin',verifyRole('ADMIN'),registerController.login);
 
-router.get('/profile',verify.verify,registerController.profile);
-router.put('/update',verify.verify,registerController.update);
+router.get('/profile',verify(['LISTER', 'ADMIN']),registerController.profile);
 
-router.put('/Update/Password',verify.verify,registerController.UpdatePassword);
+router.put('/update',verify(['LISTER', 'ADMIN']),registerController.update);
+
+router.put('/Update/Password',verify(['LISTER', 'ADMIN']),registerController.UpdatePassword);
 
 router.get('/Check/Email',registerController.EmailCheck);
 
