a9f0d16726e7c89b439afbb27b1a2ed1e6ab2b2b
Narvana <adhikarijagdish3@gmail.com>
2024-09-25 20:08:37 +0530

    146th commit -: Front route Updated Role Added in Verify Token and VerifyToken Error Updated

--- src/route/Front.route.js
+++ src/route/Front.route.js
@@ -2,21 +2,23 @@ const express = require('express');
 
 const router=express.Router();
 
-const verifyToken=require('../middleware/token/verifyToken');
+const {verify}=require('../middleware/token/verifyToken');
 
-const {verifyRoles}=require('../middleware/role/verifyRole');
-
-const upload=require('../middleware/ImageUpload/imageUploadMiddleware')
 
 const FrontController=require('../controller/Front.controller');
 
 router.get('/get/All/Listing',FrontController.AllListing)
+
 // router.get('/get/All/Verified',FrontController.AllVerified);
+
 router.get('/get/All/Featured',FrontController.AllFeatured);
+
 router.get('/get/Search',FrontController.searchWareHouseAll);
+
 router.get('/get/Recent/Warehouse',FrontController.recentWarehouse);
-router.post('/Send/Enquiry',verifyToken.verify,FrontController.SendEnquiry);
-// router.post('/add/ThreePL',verifyToken.verify,verifyRoles(['WAREHOUSE', 'ADMIN']),upload.fields([{ name: 'warehouse_details[WarehouseImage]', maxCount: 5 }]),warehouseController.Add3PL);
+
+router.post('/Send/Enquiry',verify(['LISTER', 'ADMIN']),FrontController.SendEnquiry);
+
 router.post('/Send/Requirement',FrontController.SendRequirement);
 
 module.exports=router
