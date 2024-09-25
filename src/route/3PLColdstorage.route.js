54345ff6e266191e24f6f84fe3bdbb6f1e2e7325
Narvana <adhikarijagdish3@gmail.com>
2024-09-25 18:03:14 +0530

    141th commit -: 3PL Cold Storage route Updated Role Added in Verify Token and VerifyToken Error Updated

--- src/route/3PLColdstorage.route.js
+++ src/route/3PLColdstorage.route.js
@@ -2,19 +2,26 @@ const express = require('express');
 
 const router=express.Router();
 
-const verifyToken=require('../middleware/token/verifyToken');
-
-const {verifyRoles}=require('../middleware/role/verifyRole');
+const {verify}=require('../middleware/token/verifyToken');
 
 const upload=require('../middleware/ImageUpload/imageUploadMiddleware');
 
 const PLColdStorageController=require('../controller/3PLColdStorage.controller');
 
 // 3PL ColdStorage
-router.post('/add/ThreePL/Coldstorage',verifyToken.verify,verifyRoles(['LISTER', 'ADMIN']),upload.fields([{name:'cold_storage_details[ColdStorageImage]'}]),PLColdStorageController.Add3PLColdStorage);
-router.get('/get/3PL/ColdStorage/Lister/All',verifyToken.verify,PLColdStorageController.AllPLColdStorage);
+// Add 3PL Coldstorage
+router.post('/add/ThreePL/Coldstorage',verify(['LISTER', 'ADMIN']),upload.fields([{name:'cold_storage_details[ColdStorageImage]'}]),PLColdStorageController.Add3PLColdStorage);
+
+// GET LISTER ALL 3PL Coldstorage
+router.get('/get/3PL/ColdStorage/Lister/All',verify(['LISTER', 'ADMIN']),PLColdStorageController.AllPLColdStorage);
+
+// GET Single 3PL ColdStorage With provided ID
 router.get('/get/3PL/ColdStorage/Lister/Single',PLColdStorageController.singlePLColdStorage);
-router.put('/update/3PL/ColdStorage',verifyToken.verify,PLColdStorageController.UpdatePLColdStorage);
-router.delete('/delete/3PL/ColdStorage',verifyToken.verify,PLColdStorageController.DeletePLColdStorage);
+
+// UPDATE 3PL ColdStorage Provide ID
+router.put('/update/3PL/ColdStorage',verify(['LISTER', 'ADMIN']),PLColdStorageController.UpdatePLColdStorage);
+
+// DELETE 3PL ColdStorage Provide ID
+router.delete('/delete/3PL/ColdStorage',verify(['LISTER', 'ADMIN']),PLColdStorageController.DeletePLColdStorage);
 
 module.exports=router
