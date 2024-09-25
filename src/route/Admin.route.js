528dfc7aa396c71b156054a532f5366cf1bcdac2
Narvana <adhikarijagdish3@gmail.com>
2024-09-25 20:11:27 +0530

    149th commit -: Admin route Updated Role Defined properly in add listing

--- src/route/Admin.route.js
+++ src/route/Admin.route.js
@@ -2,32 +2,31 @@ const express = require('express');
 
 const router=express.Router();
 
-const verifyToken=require('../middleware/token/verifyToken');
+const {verify}=require('../middleware/token/verifyToken');
 
-const {verifyRole,verifyRoles}=require('../middleware/role/verifyRole');
-
-// const upload=require('../middleware/ImageUpload/imageUploadMiddleware')
+// const {verifyRoles}=require('../middleware/role/verifyRole');
 
 const AdminController=require('../controller/Admin.controller');
 
 // All Warehouse and 3PL
-router.get('/get/All/Listing',AdminController.allListing);
+router.get('/get/All/Listing',verify(['ADMIN']),AdminController.allListing);
 
-router.put('/Verified/Status',verifyToken.verify,verifyRole('ADMIN'),AdminController.UpdateVerifiedStatus);
+router.put('/Verified/Status',verify(['ADMIN']),AdminController.UpdateVerifiedStatus);
 
-router.put('/Featured/Status',verifyToken.verify,verifyRole('ADMIN'),AdminController
-.UpdateFeatureStatus);
+router.put('/Featured/Status',verify(['ADMIN']),AdminController.UpdateFeatureStatus);
 
-router.get('/Enquiry/List',AdminController.EnquiryList);
+router.get('/Enquiry/List',verify(['ADMIN']),AdminController.EnquiryList);
 
-router.get('/Listers/List',AdminController.ListerList);
+router.get('/Listers/List',verify(['ADMIN']),AdminController.ListerList);
 
-router.delete('/Remove/Enquiry',verifyToken.verify,verifyRole('ADMIN'),AdminController.RemoveEnquiry);
+router.delete('/Remove/Enquiry',verify(['ADMIN']),AdminController.RemoveEnquiry);
 
-router.delete('/Remove/Listing',verifyToken.verify,verifyRole('ADMIN'),AdminController.RemoveListing);
+router.delete('/Remove/Listing',verify(['ADMIN']),AdminController.RemoveListing);
 
-router.get('/Requirement/List',AdminController.RequirementList);
+router.get('/Requirement/List',verify(['ADMIN']),AdminController.RequirementList);
 
-router.delete('/Remove/Requirement',verifyToken.verify,verifyRole('ADMIN'),AdminController.RemoveRequirement);
+router.delete('/Remove/Requirement',verify(['ADMIN']),AdminController.RemoveRequirement);
+
+router.delete('/Remove/Lister',verify(['ADMIN']),AdminController.RemoveLister);
 
 module.exports=router
