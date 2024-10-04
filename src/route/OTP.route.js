6ee3bf250e7241f85ef39e4fadfd41f02bc23558
Narvana <adhikarijagdish3@gmail.com>
2024-10-04 23:17:14 +0530

    165th commit -: OTP Route Updated

--- src/route/OTP.route.js
+++ src/route/OTP.route.js
@@ -2,10 +2,14 @@ const express = require('express');
 // const OtpModel = require("../model/");
 const router = express.Router();
 
+const definerole=require('../middleware/role/defineRole');
+
 const {verifyRole}=require('../middleware/role/verifyRole')
 const {requestOtp,verifyOtp}=require("../controller/OTP.controller");
+const defineRole = require('../middleware/role/defineRole');
 
-router.post('/request-otp', requestOtp);
+router.post('/request-otp',defineRole('LISTER'), requestOtp);
+
 router.post('/verify-otp',verifyRole('LISTER'),verifyOtp);
 
 module.exports = router;
